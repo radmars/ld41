be54504848a7e5e3475ec88ec61a581b61ec5031
Eugene Marcotte <emarcotte@gmail.com>
2018-04-21 11:39:33 -0400

    Initial stuffs

---
+++ Assets/Flipper.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Flipper : MonoBehaviour {
+
+	public enum SideEnum {
+		Left,
+		Right
+	}
+
+	public SideEnum side;
+	private string key;
+
+	// Use this for initialization
+	void Start () {
+		key = side == SideEnum.Left ? "Left" : "Right";
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(key)) {
+		
+		}
+	}
+}
