a947bf032488607dd2488d10ecb333a521aded3d
Adhesion <adhesionmusic@gmail.com>
2018-04-21 18:22:27 -0400

    Add slingshots

--- Assets/Structures/Bumper.cs
+++ Assets/Structures/Bumper.cs
@@ -5,12 +5,22 @@ using UnityEngine;
 public class Bumper : MonoBehaviour {
 	public float force = 500.0f;
 
+	public bool useOverrideVector = false;
+	public Vector2 overrideVector;
+
 	public void OnCollisionEnter2D(Collision2D collision) {
 		if (collision.gameObject.tag == "ball") {
-			Vector3 diff = collision.gameObject.transform.position - transform.position;
-			diff.Normalize();
-			Vector2 forceVector = new Vector2(diff.x, diff.y);
-			forceVector *= force;
+			Vector2 forceVector;
+
+			if (useOverrideVector) {
+				forceVector = overrideVector * force;
+			} else {
+				Vector3 diff = collision.gameObject.transform.position - transform.position;
+				diff.Normalize();
+				forceVector = new Vector2(diff.x, diff.y);
+				forceVector *= force;
+			}
+
 			collision.gameObject.GetComponent<Rigidbody2D>().AddForce(forceVector);
 		}
 	}
