0cfc473898da121fcf7fce19e0af75fbd24745b1
Eugene Marcotte <emarcotte@gmail.com>
2018-04-22 12:09:33 -0400

    Build spots

---
+++ Assets/BuildOption.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class BuildOption : MonoBehaviour {
+
+	public GameObject buildPrefab;
+	private BuildSpot spot;
+	private bool over;
+
+	public void SetBuildSpot(BuildSpot s) {
+		spot = s;
+	}
+
+	public void OnMouseEnter() {
+		over = true;
+	}
+
+	public void OnMouseExit() {
+		over = false;
+	}
+
+	public void OnMouseDown() {
+		if(over) {
+			spot.Build(Instantiate(buildPrefab));
+		}
+	}
+}
---
+++ Assets/BuildSpot.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class BuildSpot : MonoBehaviour {
+
+	Color mouseOverColor = Color.red;
+	Color originalColor;
+	new MeshRenderer renderer;
+
+	private BuildOption[] options;
+	private bool over;
+
+	void Start()
+	{
+		renderer = GetComponent<MeshRenderer>();
+		originalColor = renderer.material.color;
+		options = GetComponentsInChildren<BuildOption>();
+		foreach(var o in options) {
+			o.gameObject.SetActive(false);
+			o.SetBuildSpot(this);
+		}
+	}
+
+	public void OnMouseEnter() {
+		over = true;
+		renderer.material.color = mouseOverColor;
+			}
+
+	public void OnMouseDown() {
+		if(over) {
+			foreach(var o in options) {
+				o.gameObject.SetActive(!o.gameObject.activeInHierarchy);
+			}
+		}
+	}
+
+	public void OnMouseExit() {
+		over = false;
+		renderer.material.color = originalColor;
+	}
+
+	public void Build(GameObject prefab) {
+		prefab.transform.parent = transform.parent;
+		prefab.transform.position = transform.position;
+		Destroy(gameObject);
+	}
+}
