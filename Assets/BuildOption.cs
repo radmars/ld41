7fc93496f62f116901075e7d2e2247fcff23f45b
Eugene Marcotte <emarcotte@gmail.com>
2018-04-23 18:23:47 -0400

    Buttons for buying stuff show cost and dim to red when you cant afford

--- Assets/BuildOption.cs
+++ Assets/BuildOption.cs
@@ -5,6 +5,10 @@ public class BuildOption : MonoBehaviour {
 	public GameObject buildPrefab;
 	private BuildSpot spot;
 	private bool over;
+	private TextMesh text;
+
+	public int mineralCost = 10;
+	public int gasCost = 20;
 
 	public void SetBuildSpot(BuildSpot s) {
 		spot = s;
@@ -18,9 +22,22 @@ public class BuildOption : MonoBehaviour {
 		over = false;
 	}
 
+	public void Awake() {
+		text = GetComponentInChildren<TextMesh>();
+	}
+
+	public void Update() {
+		text.text = string.Format("{0}M\n{1}G", mineralCost, gasCost);
+		text.color = ResourceController.instance.Minerals >= mineralCost && ResourceController.instance.Gas >= gasCost ? Color.white : Color.red;
+	}
+
 	public void OnMouseDown() {
 		if(over) {
-			spot.Build(Instantiate(buildPrefab));
+			if(ResourceController.instance.Minerals >= mineralCost && ResourceController.instance.Gas >= gasCost) {
+				ResourceController.instance.Minerals -= mineralCost;
+				ResourceController.instance.Gas -= gasCost;
+				spot.Build(Instantiate(buildPrefab));
+			}
 		}
 	}
 }
